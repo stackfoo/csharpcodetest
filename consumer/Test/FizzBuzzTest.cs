c9ac64bab50752b7eb85cb147e71eed2182bf13b
James <james@jamespritz.com>
2014-03-09 11:42:58 -0400

    Add Unit Test

---
+++ consumer/Test/FizzBuzzTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Xunit;
+
+namespace Test {
+
+    public class FizzBuzzTest {
+        [Fact]
+        public void TestFizz() {
+
+            throw new NotImplementedException();
+
+        }
+    }
+}
